503225bd684c98da59d9a42dcacad687faff462f
Sav <40448048+savdaddy@users.noreply.github.com>
2018-11-12 13:04:14 -0800

    adding myLandmark hbs view

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,33 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
+        title: 'Home Page',
+        style: 'styles.css',
         examples: dbExamples
       });
     });
   });
 
+  // Load saved-landmarks page
+  app.get("/saved", function (req, res) {
+    res.render("landmarks", {
+      title: 'My Landmarks',
+      style: 'landmarks.css',
+    });
+  });
+
+
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -20,7 +35,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
