03be33f90f7f8f1198d0104f20712ee19d09d563
Sav <40448048+savdaddy@users.noreply.github.com>
2018-11-22 11:47:58 -0800

    favorites button animation added

--- public/js/search.js
+++ public/js/search.js
@@ -7,3 +7,18 @@ $("#submitInput").on("click", function (event) {
 
    window.location.replace(baseURL + "/search/" + locationInput);
 });
+
+// Favorites button hook
+$(".fa-star").click(function() {
+   $(this).toggleClass('active');
+});
+
+/* When a user clicks, toggle the 'is-animating' class */
+$(".fa-star").on('click touchstart', function() {
+   $(this).toggleClass('is_animating');
+});
+
+/* When the animation is over, remove the class*/
+$(".fa-star").on('animationend', function(){
+   $(this).toggleClass('is_animating');
+ });
